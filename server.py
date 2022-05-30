cf33b3c6d895b4e6b3d98fdc832ac975236722d3
jacexavinbello@gmail.com <Xavin616>
2022-05-30 21:34:49 +0100

    Finally got global and local state.

--- server.py
+++ server.py
@@ -5,13 +5,16 @@ import json, time
 xorin = bajeh_api.get_user(screen_name="Lostbj")
 print(xorin.name, '\n')
 
-with open('art.json', 'r+') as prev:
-    prv_art = dict(json.load(prev))
-
-new = get_article()
-
 while True:
-    if prv_art != new:
+    new = get_article()
+
+    with open('art.json', 'r+') as prev:
+        prv_art = dict(json.load(prev))
+
+    print("New: ", new['title'])
+    print("Previuos: ", prv_art['title'])
+    
+    if prv_art['title'] != new['title']:
         print('Posting New Article:', new['title'])
         tweet = f"{new['title']}\nRead more:  {str(new['link'])}"
         img = new['image_url']
@@ -19,8 +22,8 @@ while True:
         with open('art.json', 'w') as f:
             json.dump(new, f, indent=4)
         print("Added new article to json file.\n\nWaiting....")
-        time.sleep(900)
+        time.sleep(600)
     else: 
         print("News not updated yet!\nWaiting....")
-        time.sleep(900)
+        time.sleep(600)
         continue
